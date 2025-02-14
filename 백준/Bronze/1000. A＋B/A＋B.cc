0003a8b67a2b2c34bed6a155c8465506f72dcd0c
yhc5091 <yhc5091@gmail.com>
2025-02-14 20:29:19 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int a,b;
+    cin >> a;
+    cin >> b;
+    cout << a+b << endl;
+    
+    return 1;
+
+    return 0;
+}
