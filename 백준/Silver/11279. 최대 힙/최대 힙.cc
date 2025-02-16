094254c04f2975c7bc783192c2b135a1980d8fd7
yhc5091 <yhc5091@gmail.com>
2025-02-16 13:57:08 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+	cin.tie(0);
+
+    int N;
+    cin >> N;
+
+    priority_queue<int> pq;
+    for(int i =0;i < N; i++) {
+        int x;
+        cin >> x;
+
+        if(x > 0) {
+            pq.push(x);
+        }
+        else {
+            if(pq.empty()) {
+                cout << "0\n";
+            }
+            else
+            {
+                cout << pq.top() << "\n";
+                pq.pop();
+            }
+        }
+    }
+
+    
+    return 0;
+}
