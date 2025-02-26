82bcccba31a10bf16d57c68a27caae81047ff2ec
yhc5091 <yhc5091@gmail.com>
2025-02-26 21:04:18 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{    
+    ios::sync_with_stdio(false);
+	cin.tie(0);
+    
+    int A, B;
+    cin >> A >> B;
+
+    cout << A + B;
+
+    return 0;
+}
