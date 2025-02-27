83fb3bbe650c6869017a00432ddc95760d82012f
yhc5091 <yhc5091@gmail.com>
2025-02-27 16:53:39 +0900

    [Bronze V] Title: 크냐?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{    
+    ios::sync_with_stdio(false);
+	cin.tie(0);
+    
+    while(true) {
+        int a, b;
+
+        cin >> a >> b;
+        if(a == 0 && b == 0) break;
+
+        if(a > b) cout << "Yes\n";
+        else cout << "No\n";
+
+    }
+
+
+    return 0;
+}
