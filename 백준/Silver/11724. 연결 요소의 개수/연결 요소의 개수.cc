76556f9564569c0abc723e2c22b6f50657684a25
yhc5091 <yhc5091@gmail.com>
2025-03-04 14:32:19 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 76 ms, Memory: 5980 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int** g;
+int N, M;
+int* c;
+
+bool search(int t) {
+    // cout << "search : " << t << "\n";
+    bool exist = false;
+    for(int i = 1; i <= N; i++) {
+        if(g[t][i] == 1) {
+            // cout << "(" << t << "," << i << ")\n";
+            c[t] = 1;
+            if(c[i] == 0) search(i);
+            c[i] = 1;
+            exist = true;
+        }
+    }
+    return exist;
+}
+
+int main()
+{    
+    ios::sync_with_stdio(false);
+	cin.tie(0);
+    
+    cin >> N >> M;
+
+    g = new int*[N+1];
+    for(int i = 0; i < N+1; i++) g[i] = new int[N+1] {0};
+
+    for(int i = 1; i <= M; i++) {
+        int u, v;
+        cin >> u >> v;
+    
+        g[u][v] = 1;
+        g[v][u] = 1;
+    }
+
+    int s = 0;
+    c = new int[N+1] {0};
+
+    for(int i = 1 ; i <= N; i++) {
+        if(c[i] == 0) {
+            // cout << i << " not checked\n";
+            s++;
+        }
+        search(i);
+    }
+
+    cout << s << "\n";
+
+    for(int i = 0; i < N+1; i++) delete[] g[i];
+    delete[] g;
+    delete[] c;
+
+    return 0;
+}
